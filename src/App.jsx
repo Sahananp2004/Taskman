fb1e19bf4a3db78b79a0c5ed2c2968312b1085f9
Sahana NP <sahananp64@gmail.com>
2025-11-21 16:23:40 +0530

    work continued

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import { useState,useEffect } from 'react';
 import Taskform from './Components/Taskform'
 import Tasklist from './Components/Tasklist'
 import Progresstracker from './Components/Progresstracker'
 
-export default function App() {
-  const[tasks, setTasks] = useState([]);
 
-  const addTask = (task) => {
-    setTasks(task);
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
   }
+
+
   return (
     <div>
       <header>
